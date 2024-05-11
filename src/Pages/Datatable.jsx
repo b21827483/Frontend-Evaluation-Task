83efd27fb43297fc32c7c6d604680b51126efeed
b21827483 <b21827483@cs.hacettepe.edu.tr>
2024-05-11 14:44:37 +0300

    Searchbar UI added

--- src/Pages/Datatable.jsx
+++ src/Pages/Datatable.jsx
@@ -1,6 +1,7 @@
 import { Container } from "@mui/material";
 import Navbar from "../components/Shared/Navbar";
 import Line from "../components/Shared/UI/Line";
+import SearchBar from "../components/Shared/SearchBar";
 
 function Datatable() {
     
@@ -8,6 +9,7 @@ function Datatable() {
         <Container sx={{width: "100%"}} disableGutters maxWidth={false}>
             <Navbar />
             <Line />
+            <SearchBar />
         </Container>
     )
 }
